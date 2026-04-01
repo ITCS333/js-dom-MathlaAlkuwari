88d4a9448b8a94e077b94db6b7607bdcaeb729b0
MathlaAlkuwari <mathlaalkuwari.23@gmail.com>
2026-04-01 19:34:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Selects the element with the id 'main-heading' and changes its text content to 'DOM Manipulation Challenge'.
  */
 function changeHeadingText() {
-  document.getElementById("main-heading").textContent ="DOM Manpulation Challenge";
+ document.getElementById("main-heading").textContent = "DOM Manipulation Challenge";
 }
 
 /**
@@ -11,29 +11,27 @@ function changeHeadingText() {
 function changeBoxColor() {
   document.getElementById("box-to-modify").style.backgroundColor = "lightblue";
 }
-
 /**
  * Creates a new <li> element, sets its text content to 'New Item', and appends it to the <ul> with the id 'item-list'.
  */
 function addNewItem() {
- const li = document.createElement("li");
- li.textContent ="New Item";
-
- document.getElementById("item-list").appendChild(li)
+   const li = document.createElement("li");
+    li.textContent = "New Item";
+    document.getElementById("item-list").appendChild(li);
 }
 
 /**
  * Selects the paragraph with the class 'content-para' and adds the class 'highlight' to it.
  */
 function highlightParagraph() {
- document.querySelector(".content-para").classList.add("highlight");
+document.querySelector(".content-para").classList.add("highlight");
 }
 
 /**
  * Selects the element with the id 'to-be-removed' and removes it from the DOM.
  */
 function removeElement() {
-    const el = document.getElementById("to-be-removed");
+   const el = document.getElementById("to-be-removed");
     el.remove();
 }
 
